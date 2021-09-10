309ab6df504a9f03398fd6b6184688efb587f7cf
Unrike <vladik-sto@ya.ru>
2021-09-10 14:08:35 +0300

    Lesson 2 completed!

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -18,7 +18,14 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val p = number / 1000
+    val v = number / 100 - p * 10
+    val c = number % 10
+    val t = (number % 100 - c) / 10
+    if (p + v == t + c) return true
+    else return false
+}
 
 /**
  * Простая (2 балла)
@@ -27,7 +34,9 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    return (x1 == x2) || (y1 == y2) || (kotlin.math.abs(x1 - x2) == kotlin.math.abs(y1 - y2))
+}
 
 
 /**
@@ -36,7 +45,11 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int = when (month) {
+    4, 6, 9, 11 -> 30
+    2 -> if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) 29 else 28
+    else -> 31
+}
 
 /**
  * Простая (2 балла)
@@ -48,7 +61,10 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean {
+    val s = kotlin.math.sqrt(sqr((x2 - x1)) + sqr(y2 - y1))
+    return s + r1 <= r2
+}
 
 /**
  * Средняя (3 балла)
